c1fb7bb488c8082a5b269b9f9278ef1a1ae8093b
Fatimata Ndiaye <2006fatimatandiaye@gmail.com>
2026-05-03 20:49:49 +0000

    Lab 2 : composants statiques LigneBus et ListeLignes

---
+++ src/Statistique3.js
@@ -0,0 +1,12 @@
+import './Statistique.css';
+
+function Statistique3() {
+  return (
+    <div className="statistique">
+      <span className="stat-chiffre">5</span>
+      <span className="stat-libelle">communes</span>
+    </div>
+  );
+}
+
+export default Statistique3;
