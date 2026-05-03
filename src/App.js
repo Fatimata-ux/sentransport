e13e7874e1443aa163c9f263bab03428cdd27e96
Fatimata Ndiaye <2006fatimatandiaye@gmail.com>
2026-05-03 22:10:36 +0000

    Lab 2 : composants statistiques LigneBus et ListeLignes

--- src/App.js
+++ src/App.js
@@ -2,21 +2,27 @@ import './App.css';
 import Header from './Header';
 import ListeLignes from './ListeLignes';
 import Footer from './Footer';
+import StatReseau from './StatReseau';
 
 function App() {
   const lignes = [
-    { id: 1, numero: "1", depart: "Parcelles Assainies", arrivee: "Plateau", arrets: 14 },
-    { id: 2, numero: "7", depart: "Guediavaye", arrivee: "Place Obe", arrets: 18 },
-    { id: 3, numero: "15", depart: "Pikine", arrivee: "Medina", arrets: 12 },
-    { id: 4, numero: "23", depart: "Ouakam", arrivee: "Grand Dakar", arrets: 10 },
-    { id: 5, numero: "8", depart: "Almadies", arrivee: "Colobane", arrets: 16 },
-    { id: 6, numero: "12", depart: "Yoff", arrivee: "Sandaga", arrets: 11 },
-  ];
+    { id: 1, numero: "1", depart: "Parcelles Assainies", arrivee: "Plateau", arrets: 14, couleur: "#0a6e31" },
+    { id: 2, numero: "7", depart: "Guediavaye", arrivee: "Place Obe", arrets: 18, couleur: "#e74c3c" },
+    { id: 3, numero: "15", depart: "Pikine", arrivee: "Medina", arrets: 12, couleur: "#e67e22" },
+    { id: 4, numero: "23", depart: "Ouakam", arrivee: "Grand Dakar", arrets: 10, couleur: "#8e44ad" },
+    { id: 5, numero: "8", depart: "Almadies", arrivee: "Colobane", arrets: 16, couleur: "#2980b9" },
+    { id: 6, numero: "12", depart: "Yoff", arrivee: "Sandaga", arrets: 11, couleur: "#c0392b" },
+    { id: 7, numero: "26", depart: "Fann", arrivee: "Liberte", arrets: 9, couleur: "#16a085" },
+    { id: 8, numero: "31", depart: "Medina", arrivee: "HLM", arrets: 7, couleur: "#d35400" },
+    { id: 9, numero: "42", depart: "Dieuppeul", arrivee: "Sandaga", arrets: 13, couleur: "#27ae60" },
+    { id: 10, numero: "18", depart: "Sicap", arrivee: "Plateau", arrets: 15, couleur: "#2c3e50" },
+];
 
   return (
     <div className="App">
       <Header />
       <main className="contenu">
+        <StatReseau lignes={lignes} />
         <ListeLignes lignes={lignes} />
       </main>
       <Footer />
